e95bddc1b22bf5ab3b9c2f05860fa1922190dccb
Swaroop C H <swaroop@swaroopch.com>
2012-12-04 13:36:36 +0530

    Open and Save file now works

--- index.js
+++ index.js
@@ -3,29 +3,59 @@ $(function() {
     var fs = require("fs");
     var exec = require("exec");
 
-    var tmp_source_file = os.tmpDir() + "kalam.txt";
+    var tmpSourceFile = os.tmpDir() + "kalam.txt";
 
     function render() {
         var text = $("#source-text").val();
 
-        fs.writeFile(tmp_source_file, text, function(err) {
+        fs.writeFile(tmpSourceFile, text, function(err) {
             if (err) {
                 alert(err);
                 return;
             }
 
-            var pandoc_command = ["pandoc",
+            var pandocCommand = ["pandoc",
                                     "-f",
                                     "markdown",
                                     "-t",
                                     "html5",
                                     "-i",
-                                    tmp_source_file];
-            exec(pandoc_command, function(err, out, code) {
+                                    tmpSourceFile];
+            exec(pandocCommand, function(err, out, code) {
                 $("#preview-html").html(out);
             });
         });
     }
 
     window.setInterval(render, 3000);
+
+    function readFile(filename) {
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                alert(err);
+                return;
+            }
+
+            $("#source-text").val(data);
+        });
+    }
+
+    function writeFile(filename, content) {
+        fs.writeFile(filename, content, "utf8", function() {
+            $("#save-success-dialog").reveal();
+        });
+    }
+
+    $("#choose-file").on("change", function(eventData) {
+        var filename = $("#choose-file").val();
+        readFile(filename);
+    });
+
+    $(".save-to-file").on("click", function() {
+        var filename = $("#choose-file").val();
+        if (!filename || filename === "") {
+            return;
+        }
+        writeFile(filename, $("#source-text").val());
+    });
 });
